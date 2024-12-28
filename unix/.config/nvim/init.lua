47329b9685d248287a537ce45467be3f5bbfa879
James Chan <jameschan312.cn@gmail.com>
2024-12-28 16:06:17 -0500

    nvim

--- unix/.config/nvim/init.lua
+++ unix/.config/nvim/init.lua
@@ -1,7 +1,14 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+-- bootstrap lazy.nvim, LazyVim and plugins
 require("config.lazy")
 
+-- Enable transparent background
 vim.cmd([[
     highlight Normal ctermbg=none guibg=none
     highlight NonText ctermbg=none guibg=none
 ]])
+
+-- Set the tab to 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.softtabstop = 4
