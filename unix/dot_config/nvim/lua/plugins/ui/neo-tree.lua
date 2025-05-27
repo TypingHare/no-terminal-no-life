2fcd278c0d15d6d5afe42cf04828f43c33263098
James Chen <jameschan312.cn@gmail.com>
2025-05-27 12:26:18 -0400

    added neo-tree keybidings; added nvim-notify

--- unix/dot_config/nvim/lua/plugins/ui/neo-tree.lua
+++ unix/dot_config/nvim/lua/plugins/ui/neo-tree.lua
@@ -10,12 +10,24 @@ return {
                 },
             },
             window = {
-                width = 30,
+                width = 32,
                 mappings = {
                     ["<space>"] = "toggle_node",
                     ["<cr>"] = "open",
                     ["S"] = "split_with_window_picker",
                     ["s"] = "vsplit_with_window_picker",
+                    ["n"] = "add",
+                    ["e"] = function()
+                        vim.cmd("wincmd l")
+                    end,
+                    ["y"] = function(state)
+                        local node = state.tree:get_node()
+                        local path = node:get_id()
+                        local rel_path = vim.fn.fnamemodify(path, ":.")
+                        vim.fn.setreg("+", rel_path)
+
+                        print("Copied relative path: " .. rel_path)
+                    end,
                 },
             },
         })
--- unix/dot_config/nvim/lua/plugins/ui/nvim-notify.lua
+++ unix/dot_config/nvim/lua/plugins/ui/nvim-notify.lua
@@ -2,5 +2,10 @@ return {
     "rcarriga/nvim-notify",
     config = function()
         vim.notify = require("notify")
+        require("notify").setup({
+            stages = "fade_in_slide_out",
+            timeout = 3000,
+            background_colour = "#000000",
+        })
     end,
 }
