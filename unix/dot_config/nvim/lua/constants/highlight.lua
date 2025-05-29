0a6581badcb8f53d3acc21dbc05ca9abf118b878
James Chen <jameschan312.cn@gmail.com>
2025-05-29 09:34:04 -0400

    Updated virt-column

---
+++ unix/dot_config/nvim/lua/constants/highlight.lua
@@ -0,0 +1,7 @@
+-- This module stores highlight group objects for some common scenarios.
+
+--- @type table<string, { fg?: string, bg?: string, bold?: boolean, italic?: boolean, underline?: boolean, undercurl?: boolean }>
+return {
+    -- Used in `indent-blankline.nvim` and `virt-column.nvim`
+    VERTICAL_BAR = { fg = '313244' },
+}
--- unix/dot_config/nvim/lua/plugins/editor/virt-column.lua
+++ unix/dot_config/nvim/lua/plugins/editor/virt-column.lua
@@ -1,25 +1,26 @@
 return {
-    "lukas-reineke/virt-column.nvim",
+    'lukas-reineke/virt-column.nvim',
     config = function()
-        require("virt-column").setup({
-            char = "▏",
-            virtcolumn = "81",
+        require('virt-column').setup {
+            char = '▏',
+            virtcolumn = '81',
             priority = 1,
-        })
+            highlight = 'VirtColumn',
+        }
 
-        vim.api.nvim_create_autocmd("ColorScheme", {
-            callback = function()
-                vim.api.nvim_set_hl(0, "ColorColumn", { fg = "#313244" })
-            end,
-        })
+        vim.api.nvim_set_hl(
+            0,
+            'VirtColumn',
+            require('constants.highlight').VERTICAL_BAR
+        )
 
         -- Disable virt-column on some filetypes
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = { "dashboard" },
+        vim.api.nvim_create_autocmd('FileType', {
+            pattern = { 'dashboard' },
             callback = function()
-                require("virt-column").setup({
-                    char = "",
-                })
+                require('virt-column').setup {
+                    char = '',
+                }
             end,
         })
     end,
