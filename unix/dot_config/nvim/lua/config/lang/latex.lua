218c86cbf93d2282a588f7c23fa61ff128dc006e
James Chen <jameschan312.cn@gmail.com>
2025-08-30 15:53:13 -0400

    Updated config

--- unix/dot_config/nvim/lua/config/lang/latex.lua
+++ unix/dot_config/nvim/lua/config/lang/latex.lua
@@ -38,6 +38,7 @@ vim.api.nvim_create_autocmd('FileType', {
     vim.opt_local.spell = true
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
+    vim.opt_local.breakindent = true
 
     vim.keymap.set('n', 'j', 'gj', { noremap = true, silent = true })
     vim.keymap.set('n', 'k', 'gk', { noremap = true, silent = true })
