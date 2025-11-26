4009e134c4e7b38e9ff0ce3a014ed19453e886f2
James <james@huliujindeMacBook-Pro.local>
2025-11-25 20:42:51 -0500

    Initial

--- modules/unix-config/dot-files/nvim/lua/config/lang/json.lua
+++ modules/unix-config/dot-files/nvim/lua/config/lang/json.lua
@@ -1,6 +1,6 @@
 require('hare-conf').fn.editor.set_lang_config('json', {
   treesitter = { name = 'json' },
-  lsp = { name = 'json-lsp' },
+  lsp = { name = 'jsonls' },
   formatter = { name = 'prettier' },
   color_column = { enabled = false },
 })
---
+++ modules/unix-config/dot-files/nvim/lua/plugin/tool/mason-tool-installer.lua
@@ -0,0 +1,5 @@
+return {
+  'WhoIsSethDaniel/mason-tool-installer.nvim',
+  event = 'VeryLazy',
+  opts = { run_on_start = false },
+}
