f0066ff5ba07e08521b6b9819987e83843e08b48
James Chen <jameschan312.cn@gmail.com>
2026-01-02 16:11:29 -0500

    updated some modules

--- modules/unix-profile/src/collect_files.zsh
+++ modules/unix-profile/src/collect_files.zsh
@@ -1,7 +1,7 @@
 # Collect all files in a directory and output it to `all.txt`
 function collect_files() {
   find "$1" -type f -name '*' -print0 | while IFS= read -r -d '' file; do
-    echo -e "\n==> $file" >>all.txt
+    echo "\n==> $file" >>all.txt
     if file --mime "$file" | grep -q 'charset=binary'; then
       echo "<binary file>" >>all.txt
     else
